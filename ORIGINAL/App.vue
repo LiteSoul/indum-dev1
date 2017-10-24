824e344f6f1eba2335cd1da63b1a3923077dd817
Ariel Blasi <exorcismo@gmail.com>
2017-10-24 17:50:43 -0300

    Initial commit PWA

---
+++ ORIGINAL/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-navigation-drawer temporary v-model="sidebar">
+    </v-navigation-drawer>
+    <v-toolbar fixed>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click.stop="sidebar = !sidebar">
+        </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    
+    <main>
+      <v-container fluid>
+      </v-container>
+    </main>
+    
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        appTitle: 'Awesome App',
+        sidebar: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
