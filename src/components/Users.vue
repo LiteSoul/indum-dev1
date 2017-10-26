54a98c1ffca6e8bf9ac27a457cb6e95c20442b10
LiteSoul <exorcismo@gmail.com>
2017-10-26 16:10:13 -0300

    up

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,36 +1,35 @@
 <template>
 
-  <v-layout row>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-btn
-          color="green"
-          dark
-          absolute
-          
-          right
-          fab
-        >
-          <v-icon>add</v-icon>
-        </v-btn>
-        <v-list two-line>
-          <template v-for="item in items">
-            <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
-            <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
-            <v-list-tile avatar v-else v-bind:key="item.name" @click="">
-              <v-list-tile-avatar>
-                <img v-bind:src="item.avatar">
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.name"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
+	<v-layout row>
+		<v-flex xs12 sm6 offset-sm3>
+			<v-card>
+				<v-btn
+					color="green"
+					dark
+					absolute
+					right
+					fab
+				>
+					<v-icon>add</v-icon>
+				</v-btn>
+				<v-list two-line>
+					<template v-for="item in items">
+						<v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+						<v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+						<v-list-tile avatar v-else v-bind:key="item.name" @click="">
+							<v-list-tile-avatar>
+								<img v-bind:src="item.avatar">
+							</v-list-tile-avatar>
+							<v-list-tile-content>
+								<v-list-tile-title v-html="item.name"></v-list-tile-title>
+								<v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+							</v-list-tile-content>
+						</v-list-tile>
+					</template>
+				</v-list>
+			</v-card>
+		</v-flex>
+	</v-layout>
 
 </template>
 
